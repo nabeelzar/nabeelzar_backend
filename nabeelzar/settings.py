c22dab1dd1f5484114d8f95c2115de1c93d8dd67
Nabeel Zar <nabeelzar@gmail.com>
2022-10-03 16:58:46 -0500

    Fix secret key issue

--- nabeelzar/settings.py
+++ nabeelzar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,8 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2pwemlo)iu!%sr^jf+)k#n5v^0u)b&x@k(r*jw22fp)%e*0pi9'
+secret = os.getenv('SECRET_KEY')
+if not secret:
+    print('SECRET_KEY environment variable not found!')
+    exit()
+
+SECRET_KEY = secret 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
